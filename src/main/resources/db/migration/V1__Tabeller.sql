2b2c35d303a2cd2df544aab69f104aae70f69cd6
sigurdgroneng <sigurdgroneng@nav.no>
2021-12-22 13:31:37 +0100

    Finish soknad table
    Co-authored-by: pjwalstrom <Per.Jorgen.Walstrom@nav.no>

--- src/main/resources/db/migration/V1__Tabeller.sql
+++ src/main/resources/db/migration/V1__Tabeller.sql
@@ -1,16 +1,29 @@
 CREATE TABLE IF NOT EXISTS person
 (
     id        BIGSERIAL PRIMARY KEY,
-    createdAt TIMESTAMP WITH TIME ZONE NOT NULL default (now() at time zone 'utc')
+    createdAt TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE IF NOT EXISTS soknad
 (
-    soker          BIGINT REFERENCES PERSON,
-    journalpostId  BIGINT                   NOT NULL,
-    dokumentinfoId BIGINT                   NOT NULL,
-    data           JSONB                    NOT NULL,
-    opprettetDato  TIMESTAMP WITH TIME ZONE NOT NULL,
-    createdAt      TIMESTAMP WITH TIME ZONE NOT NULL default (now() at time zone 'utc'),
+    soker           BIGINT REFERENCES PERSON,
+    journalpostId   BIGINT                   NOT NULL,
+    dokumentinfoId  BIGINT                   NOT NULL,
+    data            JSONB                    NOT NULL,
+    navn            VARCHAR(255),
+    opprettetDato   TIMESTAMP WITH TIME ZONE NOT NULL,
+    brukerStartDato DATE,
+    brukerSluttDato DATE,
+    systemStartDato DATE,
+    systemSluttDato DATE,
+    createdAt       TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT (NOW() AT TIME ZONE 'utc'),
     PRIMARY KEY (journalpostId, dokumentinfoId)
 );
+
+CREATE TABLE IF NOT EXISTS ident
+(
+    personId  BIGSERIAL REFERENCES person,
+    ident     VARCHAR(11),
+    identType VARCHAR,
+    createdAt TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT (NOW() AT TIME ZONE 'utc')
+)
