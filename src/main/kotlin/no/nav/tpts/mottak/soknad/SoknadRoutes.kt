041870ad640c4cb0cd40c87661a723988648c14d
Per Jørgen Walstrøm <per.jorgen.walstrom@nav.no>
2021-12-23 14:21:00 +0100

    Serialize/deserialize dates from/to database
    Co-authored-by: tu55eladd <Sigurd.Groneng@nav.no>

--- src/main/kotlin/no/nav/tpts/mottak/soknad/SoknadRoutes.kt
+++ src/main/kotlin/no/nav/tpts/mottak/soknad/SoknadRoutes.kt
@@ -5,8 +5,13 @@ import io.ktor.response.respond
 import io.ktor.routing.Route
 import io.ktor.routing.get
 import io.ktor.routing.route
-import kotlinx.serialization.SerialName
+import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
 import kotliquery.Connection
 import kotliquery.Row
 import kotliquery.Session
@@ -14,6 +19,7 @@ import kotliquery.queryOf
 import no.nav.tpts.mottak.db.DataSource
 import org.intellij.lang.annotations.Language
 import java.time.LocalDate
+import java.time.LocalDateTime
 import java.time.ZonedDateTime
 
 fun Route.soknadRoutes() {
@@ -31,22 +37,36 @@ fun Route.soknadRoutes() {
     }
 }
 
-
 @Serializable
 data class Soknad(
     val navn: String,
-    val opprettetDato: LocalDate,
-    val brukerStartDato: ZonedDateTime?,
-    val brukerSluttDato: ZonedDateTime?
+    @Serializable(with = LocalDateTimeSerializer::class)
+    val opprettetDato: LocalDateTime,
+    @Serializable(with = LocalDateSerializer::class)
+    val brukerStartDato: LocalDate?,
+    @Serializable(with = LocalDateSerializer::class)
+    val brukerSluttDato: LocalDate?
 ) {
     companion object {
         fun fromRow(row: Row): Soknad {
             return Soknad(
                 navn = row.string("navn"),
-                opprettetDato = row.localDate("opprettet_dato"),
-                brukerStartDato = row.zonedDateTimeOrNull("bruker_start_dato"),
-                brukerSluttDato = row.zonedDateTimeOrNull("bruker_slutt_dato")
+                opprettetDato = row.zonedDateTime("opprettet_dato").toLocalDateTime(),
+                brukerStartDato = row.localDateOrNull("bruker_start_dato"),
+                brukerSluttDato = row.localDateOrNull("bruker_slutt_dato")
             )
         }
     }
+}
+
+object LocalDateSerializer : KSerializer<LocalDate> {
+    override fun deserialize(decoder: Decoder): LocalDate = LocalDate.parse(decoder.decodeString())
+    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("LocalDate", PrimitiveKind.STRING)
+    override fun serialize(encoder: Encoder, value: LocalDate) = encoder.encodeString(value.toString())
+}
+
+object LocalDateTimeSerializer : KSerializer<LocalDateTime> {
+    override fun deserialize(decoder: Decoder) = ZonedDateTime.parse(decoder.decodeString()).toLocalDateTime()
+    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("LocalDateTime", PrimitiveKind.STRING)
+    override fun serialize(encoder: Encoder, value: LocalDateTime) = encoder.encodeString(value.toString())
 }
