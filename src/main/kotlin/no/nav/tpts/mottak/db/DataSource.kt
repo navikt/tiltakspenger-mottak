e0a0396d56f69e06509726dd39b039591e39c46c
Per Jørgen Walstrøm <per.jorgen.walstrom@nav.no>
2021-12-23 10:43:08 +0100

    Add logging of environment variables

--- src/main/kotlin/no/nav/tpts/mottak/db/DataSource.kt
+++ src/main/kotlin/no/nav/tpts/mottak/db/DataSource.kt
@@ -1,9 +1,12 @@
 package no.nav.tpts.mottak.db
 
 import com.zaxxer.hikari.HikariDataSource
+import mu.KotlinLogging
 import javax.sql.DataSource
 
+val LOG = KotlinLogging.logger {}
 fun dataSource(): DataSource {
+    LOG.info { System.getenv().keys }
     return HikariDataSource().apply {
         dataSourceClassName = "org.postgresql.ds.PGSimpleDataSource"
         password = System.getenv("NAIS_DATABASE_TPTS_TILTAKSPENGER_MOTTAK_DB_PASSWORD")
