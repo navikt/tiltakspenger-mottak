b4eedff41d0890d1729150d9fe3381649844caf0
Per Jørgen Walstrøm <per.jorgen.walstrom@nav.no>
2021-12-23 12:19:34 +0100

    Use a DataSource explicitly when configuring DataSource
    Co-authored-by: tu55eladd <Sigurd.Groneng@nav.no>

--- src/main/kotlin/no/nav/tpts/mottak/db/DataSource.kt
+++ src/main/kotlin/no/nav/tpts/mottak/db/DataSource.kt
@@ -1,18 +1,15 @@
 package no.nav.tpts.mottak.db
 
 import com.zaxxer.hikari.HikariDataSource
-import mu.KotlinLogging
+import org.postgresql.ds.PGSimpleDataSource
 import javax.sql.DataSource
 
-val LOG = KotlinLogging.logger {}
 fun dataSource(): DataSource {
-    LOG.info { System.getenv().keys }
     return HikariDataSource().apply {
-        dataSourceClassName = "org.postgresql.ds.PGSimpleDataSource"
-//        password = System.getenv("NAIS_DATABASE_TPTS_TILTAKSPENGER_MOTTAK_DB_PASSWORD")
-//        username = System.getenv("NAIS_DATABASE_TPTS_TILTAKSPENGER_MOTTAK_DB_USERNAME")
-        addDataSourceProperty("databaseName", System.getenv("NAIS_DATABASE_TPTS_TILTAKSPENGER_MOTTAK_DB_DATABASE"))
-        addDataSourceProperty("password", System.getenv("NAIS_DATABASE_TPTS_TILTAKSPENGER_MOTTAK_DB_PASSWORD"))
-        addDataSourceProperty("user", System.getenv("NAIS_DATABASE_TPTS_TILTAKSPENGER_MOTTAK_DB_USERNAME"))
+        dataSource = PGSimpleDataSource().apply {
+            databaseName = System.getenv("NAIS_DATABASE_TPTS_TILTAKSPENGER_MOTTAK_DB_DATABASE")
+        }
+        password = System.getenv("NAIS_DATABASE_TPTS_TILTAKSPENGER_MOTTAK_DB_PASSWORD")
+        username = System.getenv("NAIS_DATABASE_TPTS_TILTAKSPENGER_MOTTAK_DB_USERNAME")
     }
 }
