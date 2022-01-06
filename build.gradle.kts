ba98fe0474b3b564fe26b45b143691fbc58fb09e
Per Jørgen Walstrøm <per.jorgen.walstrom@nav.no>
2022-01-06 11:13:30 +0100

    Add gradle-dependency-analyze to warn about used/unused/declared/undeclared dependencies during build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 val javaVersion = JavaVersion.VERSION_17
 val ktorVersion = "1.6.7"
 val log4jVersion = "2.17.1"
+val kotlinxSerializationVersion = "1.3.2"
+val prometheusVersion = "0.14.1"
 
 plugins {
     application
@@ -9,6 +11,7 @@ plugins {
     id("com.github.johnrengelman.shadow") version "7.1.2"
     id("com.github.ben-manes.versions") version "0.40.0"
     id("io.gitlab.arturbosch.detekt").version("1.19.0")
+    id("ca.cutterslade.analyze").version("1.8.3")
 }
 
 repositories {
@@ -20,27 +23,42 @@ dependencies {
     // Align versions of all Kotlin components
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
     implementation(kotlin("stdlib"))
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core-jvm:1.3.2")
-    implementation("org.apache.logging.log4j:log4j-api:$log4jVersion")
-    implementation("org.apache.logging.log4j:log4j-core:$log4jVersion")
-    implementation("org.apache.logging.log4j:log4j-slf4j-impl:$log4jVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.6.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core-jvm:$kotlinxSerializationVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json-jvm:$kotlinxSerializationVersion")
+//    implementation("org.apache.logging.log4j:log4j-api:$log4jVersion")
+//    implementation("org.apache.logging.log4j:log4j-core:$log4jVersion")
+//    implementation("org.apache.logging.log4j:log4j-slf4j-impl:$log4jVersion")
     implementation("io.github.microutils:kotlin-logging-jvm:2.1.21")
-    implementation("io.ktor:ktor-server:$ktorVersion")
+//    implementation("io.ktor:ktor-server:$ktorVersion")
+    implementation("io.ktor:ktor-server-core:$ktorVersion")
     implementation("io.ktor:ktor-server-netty:$ktorVersion")
+    implementation("io.ktor:ktor-server-host-common:$ktorVersion")
+    implementation("io.ktor:ktor-http-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-utils-jvm:$ktorVersion")
     implementation("io.ktor:ktor-auth:$ktorVersion")
     implementation("io.ktor:ktor-auth-jwt:$ktorVersion")
-    implementation("io.ktor:ktor-locations:$ktorVersion")
-    implementation("io.ktor:ktor-metrics-micrometer:$ktorVersion")
+//    implementation("io.ktor:ktor-locations:$ktorVersion")
+//    implementation("io.ktor:ktor-metrics-micrometer:$ktorVersion")
     implementation("io.ktor:ktor-auth:$ktorVersion")
     implementation("io.ktor:ktor-serialization:$ktorVersion")
     implementation("io.ktor:ktor-client-serialization:$ktorVersion")
+    implementation("io.ktor:ktor-client-core:$ktorVersion")
+    implementation("io.ktor:ktor-client-core-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-client-json-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-client-serialization-jvm:$ktorVersion")
     implementation("io.ktor:ktor-client-cio:$ktorVersion")
+    implementation("io.ktor:ktor-client-cio-jvm:$ktorVersion")
     implementation("io.ktor:ktor-client-logging:$ktorVersion")
+    implementation("io.ktor:ktor-client-logging-jvm:$ktorVersion")
     implementation("no.nav.security:token-client-core:1.3.9")
+    implementation("com.nimbusds:nimbus-jose-jwt:9.15.2")
     implementation("com.auth0:java-jwt:3.18.2")
     implementation("com.auth0:jwks-rsa:0.20.0")
-    implementation("io.micrometer:micrometer-registry-prometheus:1.8.1")
-
+//    implementation("io.micrometer:micrometer-registry-prometheus:1.8.1")
+    implementation("io.prometheus:simpleclient:$prometheusVersion")
+    implementation("io.prometheus:simpleclient_common:$prometheusVersion")
+    implementation("org.jetbrains:annotations:23.0.0")
     // DB
     implementation("org.flywaydb:flyway-core:8.3.0")
     implementation("com.zaxxer:HikariCP:5.0.0")
@@ -57,6 +75,7 @@ dependencies {
         exclude(group = "ch.qos.logback", module = "logback-classic")
     }
     testImplementation("io.mockk:mockk:1.12.2")
+    testImplementation("io.mockk:mockk-dsl-jvm:1.12.2")
     testImplementation("org.skyscreamer:jsonassert:1.5.0")
 }
 
@@ -101,5 +120,13 @@ tasks {
         rejectVersionIf {
             isNonStable(candidate.version)
         }
+    }
+    analyzeClassesDependencies {
+        warnUsedUndeclared = true
+        warnUnusedDeclared = true
+    }
+    analyzeTestClassesDependencies {
+        warnUsedUndeclared = true
+        warnUnusedDeclared = true
     }
 }
