24afb5b5274e95c440357b17b0f8989be31891f7
Per Jørgen Walstrøm <per.jorgen.walstrom@nav.no>
2022-01-12 15:35:22 +0100

    Configure Kafka-consumer

--- src/main/kotlin/no/nav/tpts/mottak/joark/JoarkConsumer.kt
+++ src/main/kotlin/no/nav/tpts/mottak/joark/JoarkConsumer.kt
@@ -1,5 +1,6 @@
 package no.nav.tpts.mottak.joark
 
+import io.confluent.kafka.schemaregistry.client.SchemaRegistryClientConfig
 import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig
 import io.confluent.kafka.serializers.KafkaAvroDeserializer
 import org.apache.avro.generic.GenericRecord
@@ -7,42 +8,45 @@ import org.apache.kafka.clients.CommonClientConfigs
 import org.apache.kafka.clients.consumer.Consumer
 import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.clients.consumer.KafkaConsumer
-import org.apache.kafka.common.config.SaslConfigs
 import org.apache.kafka.common.config.SslConfigs
+import org.apache.kafka.common.security.auth.SecurityProtocol
 import org.apache.kafka.common.serialization.StringDeserializer
-import java.io.File
 import java.time.Duration
 import java.util.Properties
 
 const val MAX_POLL_RECORDS = 50
 const val SIXTY = 60
 val maxPollIntervalMs = Duration.ofSeconds(SIXTY + MAX_POLL_RECORDS * 2.toLong()).toMillis()
-val username: String = System.getenv("")
-val password: String = System.getenv("")
 
 fun createJoarkConsumer(): Consumer<String, GenericRecord> {
     return KafkaConsumer(
         Properties().also {
-            it[ConsumerConfig.GROUP_ID_CONFIG] = "tpts-tiltakspenger-mottak-v1"
+            it[ConsumerConfig.GROUP_ID_CONFIG] = "tpts-tiltakspenger-aiven-mottak-v1"
             it[ConsumerConfig.AUTO_OFFSET_RESET_CONFIG] = "earliest"
             it[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java
             it[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = KafkaAvroDeserializer::class.java
             it[ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG] = "false"
             it[ConsumerConfig.MAX_POLL_RECORDS_CONFIG] = MAX_POLL_RECORDS
             it[ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG] = "$maxPollIntervalMs"
-            it[CommonClientConfigs.SECURITY_PROTOCOL_CONFIG] = "SASL_PLAINTEXT"
+
+            it[CommonClientConfigs.SECURITY_PROTOCOL_CONFIG] = SecurityProtocol.SSL.name
+            it[SslConfigs.SSL_ENDPOINT_IDENTIFICATION_ALGORITHM_CONFIG] = ""
+            it[SslConfigs.SSL_TRUSTSTORE_TYPE_CONFIG] = "jks"
+            it[SslConfigs.SSL_KEYSTORE_TYPE_CONFIG] = "PKCS12"
+            it[SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG] = "/var/run/secrets/nais.io/kafka/client.truststore.jks"
+            it[SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG] = System.getenv("KAFKA_CREDSTORE_PASSWORD")
+            it[SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG] = "/var/run/secrets/nais.io/kafka/client.keystore.p12"
+            it[SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG] = System.getenv("KAFKA_CREDSTORE_PASSWORD")
+
             it[CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG] = System.getenv("KAFKA_BROKERS")
-            it[CommonClientConfigs.SECURITY_PROTOCOL_CONFIG] = "SASL_SSL"
-            it[SaslConfigs.SASL_MECHANISM] = "PLAIN"
-            it[SaslConfigs.SASL_JAAS_CONFIG] = "org.apache.kafka.common.security.plain.PlainLoginModule required " +
-                "username=\"$username\" password=\"$password\""
             it[AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG] = System.getenv("KAFKA_SCHEMA_REGISTRY")
-            it[SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG] = File(System.getenv("NAV_TRUSTSTORE_PATH")).absolutePath
-            it[SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG] = System.getenv("NAV_TRUSTSTORE_PASSWORD")
+            it[SchemaRegistryClientConfig.BASIC_AUTH_CREDENTIALS_SOURCE] = "USER_INFO"
+            it[SchemaRegistryClientConfig.USER_INFO_CONFIG] =
+                System.getenv("KAFKA_SCHEMA_REGISTRY_USER") + ":" + System.getenv("KAFKA_SCHEMA_REGISTRY_PASSWORD")
         }
     )
 }
 
-fun subscribeToTopic() {
-    throw NotImplementedError()
+fun subscribeToTopic(kafkaConsumer: Consumer<String, GenericRecord>, topicName: String) {
+    kafkaConsumer.subscribe(listOf(topicName))
 }
