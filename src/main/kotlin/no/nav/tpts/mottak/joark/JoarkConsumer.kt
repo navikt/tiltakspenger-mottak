7597bfd21e6874205c473b49a0fbe300e3f95571
Per Jørgen Walstrøm <per.jorgen.walstrom@nav.no>
2022-01-13 22:07:57 +0100

    Remove listener to Joark topics, should be on a branch

--- src/main/kotlin/no/nav/tpts/mottak/joark/JoarkConsumer.kt
+++ src/main/kotlin/no/nav/tpts/mottak/joark/JoarkConsumer.kt
@@ -1,31 +1,17 @@
-@file:Suppress("TooGenericExceptionCaught")
-
 package no.nav.tpts.mottak.joark
 
 import io.confluent.kafka.schemaregistry.client.SchemaRegistryClientConfig
 import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig
 import io.confluent.kafka.serializers.KafkaAvroDeserializer
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.launch
-import mu.KotlinLogging
 import org.apache.avro.generic.GenericRecord
 import org.apache.kafka.clients.CommonClientConfigs
-import org.apache.kafka.clients.consumer.Consumer
 import org.apache.kafka.clients.consumer.ConsumerConfig
-import org.apache.kafka.clients.consumer.ConsumerRecords
 import org.apache.kafka.clients.consumer.KafkaConsumer
-import org.apache.kafka.common.TopicPartition
 import org.apache.kafka.common.config.SslConfigs
-import org.apache.kafka.common.errors.WakeupException
 import org.apache.kafka.common.security.auth.SecurityProtocol
 import org.apache.kafka.common.serialization.StringDeserializer
 import java.time.Duration
 import java.util.Properties
-import kotlin.coroutines.CoroutineContext
-
-private val LOGGER = KotlinLogging.logger {}
 
 const val MAX_POLL_RECORDS = 50
 const val SIXTY = 60
@@ -57,90 +43,4 @@ fun createJoarkConsumer(topicName: String): KafkaConsumer<String, GenericRecord>
                 System.getenv("KAFKA_SCHEMA_REGISTRY_USER") + ":" + System.getenv("KAFKA_SCHEMA_REGISTRY_PASSWORD")
         }
     ).also { it.subscribe(listOf(topicName)) }
-}
-
-internal class JoarkConsumer(private val consumer: Consumer<String, GenericRecord>) : CoroutineScope {
-    override val coroutineContext: CoroutineContext
-        get() = Dispatchers.IO + Job()
-    private val job: Job = Job()
-
-    init {
-        Runtime.getRuntime().addShutdownHook(Thread(::shutdownHook))
-    }
-
-    fun start() {
-        LOGGER.info("starting JournalfoeringReplicator")
-        launch {
-            run()
-        }
-    }
-
-    private fun stop() {
-        LOGGER.info("stopping JournalfoeringReplicator")
-        consumer.wakeup()
-        job.cancel()
-    }
-
-    private fun run() {
-        try {
-            while (job.isActive) {
-                onRecords(consumer.poll(Duration.ofSeconds(1)))
-            }
-        } catch (e: WakeupException) {
-            if (job.isActive) throw e
-        } catch (e: Exception) {
-            LOGGER.error(e) { "Noe feil skjedde i konsumeringen" }
-            throw e
-        } finally {
-            closeResources()
-        }
-    }
-
-    private fun onRecords(records: ConsumerRecords<String, GenericRecord>) {
-        LOGGER.debug { "onrecords: ${records.count()}" }
-        if (records.isEmpty) return // poll returns an empty collection in case of rebalancing
-        val currentPositions = records
-            .groupBy { TopicPartition(it.topic(), it.partition()) }
-            .mapValues { partition -> partition.value.minOf { it.offset() } }
-            .toMutableMap()
-        try {
-            records.onEach { record ->
-                val tema = record.value().get("temaNytt")?.toString() ?: ""
-                if (tema == "IND") {
-                    LOGGER.info { "Mottok tema '$tema'. $record" }
-                }
-                currentPositions[TopicPartition(record.topic(), record.partition())] = record.offset() + 1
-            }
-        } catch (err: Exception) {
-            LOGGER.info(
-                "due to an error during processing, positions are reset to " +
-                    "each next message (after each record that was processed OK):" +
-                    currentPositions.map { "\tpartition=${it.key}, offset=${it.value}" }
-                        .joinToString(separator = "\n", prefix = "\n", postfix = "\n"),
-                err
-            )
-            currentPositions.forEach { (partition, offset) -> consumer.seek(partition, offset) }
-            throw err
-        } finally {
-            consumer.commitSync()
-        }
-    }
-
-    private fun closeResources() {
-        tryAndLog(consumer::unsubscribe)
-        tryAndLog(consumer::close)
-    }
-
-    private fun tryAndLog(block: () -> Unit) {
-        try {
-            block()
-        } catch (err: Exception) {
-            LOGGER.error(err.message, err)
-        }
-    }
-
-    private fun shutdownHook() {
-        LOGGER.info("received shutdown signal, stopping app")
-        stop()
-    }
 }
