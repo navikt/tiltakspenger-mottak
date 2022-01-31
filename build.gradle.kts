c828cbb63ad20642fc0407a00564b94fd7733384
Per Jørgen Walstrøm <per.jorgen.walstrom@nav.no>
2022-01-31 10:06:54 +0100

    Bump nimbus-jose-jwt [9.16.1 -> 9.17]

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,7 +54,7 @@ dependencies {
     implementation("io.ktor:ktor-client-logging:$ktorVersion")
     implementation("io.ktor:ktor-client-logging-jvm:$ktorVersion")
     implementation("no.nav.security:token-client-core:1.3.10")
-    implementation("com.nimbusds:nimbus-jose-jwt:9.16.1")
+    implementation("com.nimbusds:nimbus-jose-jwt:9.17")
     implementation("com.auth0:java-jwt:3.18.3")
     implementation("com.auth0:jwks-rsa:0.20.1")
 //    implementation("io.micrometer:micrometer-registry-prometheus:1.8.1")
