6416228ccaeff0f8d34c0da24911ced2ade6e1eb
sigurdgroneng <sigurdgroneng@nav.no>
2022-02-14 12:34:47 +0100

    Reuse Azure token if not expired

--- src/main/kotlin/no/nav/tpts/mottak/clients/AzureOauthClient.kt
+++ src/main/kotlin/no/nav/tpts/mottak/clients/AzureOauthClient.kt
@@ -10,6 +10,7 @@ import kotlinx.serialization.Serializable
 import no.nav.security.token.support.client.core.OAuth2GrantType
 import no.nav.security.token.support.client.core.OAuth2ParameterNames
 import no.nav.tpts.mottak.clients.HttpClient.client
+import java.time.LocalDateTime
 
 private val wellknownUrl = System.getenv("AZURE_APP_WELL_KNOWN_URL")
 private val clientSecret = System.getenv("AZURE_APP_CLIENT_SECRET")
@@ -21,13 +22,24 @@ private val scope = System.getenv("SCOPE") ?: "api://dev-fss.teamdokumenthandter
 object AzureOauthClient {
     private val wellknown: WellKnown by lazy { runBlocking { client.get(wellknownUrl) } }
 
-    suspend fun getToken(): OAuth2AccessTokenResponse {
-        return client.submitForm(
+    private val tokenCache = TokenCache()
+
+    suspend fun getToken(): String {
+        val currentToken = tokenCache.token
+        if (currentToken != null && !tokenCache.isExpired()) return currentToken
+
+        return client.submitForm<OAuth2AccessTokenResponse>(
             url = wellknown.tokenEndpoint,
             formParameters = Parameters.build {
                 appendToken()
             }
-        )
+        ).let {
+            tokenCache.update(
+                it.accessToken,
+                it.expiresIn.toLong()
+            )
+            return@let it.accessToken
+        }
     }
 
     suspend fun onBehalfOfExchange(accessToken: String): OAuth2AccessTokenResponse {
