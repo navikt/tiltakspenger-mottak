e2fad09ccb212a4f9010514426a63fac45ed9ee3
Per Jørgen Walstrøm <per.jorgen.walstrom@nav.no>
2022-03-22 19:55:25 +0100

    Change group-id towards Joark Kafka to receive old messages

--- src/main/kotlin/no/nav/tpts/mottak/db/queries/PersonQueries.kt
+++ src/main/kotlin/no/nav/tpts/mottak/db/queries/PersonQueries.kt
@@ -1,15 +1,18 @@
 package no.nav.tpts.mottak.db.queries
 
 import kotliquery.queryOf
+import mu.KotlinLogging
 import no.nav.tpts.mottak.db.DataSource.session
 import org.intellij.lang.annotations.Language
 
 object PersonQueries {
+    private val LOG = KotlinLogging.logger {}
+
     @Language("SQL")
     private val insert = "insert into person (ident, fornavn, etternavn) values (:ident, :fornavn, :etternavn)"
 
     @Language("SQL")
-    private val existsQuery = "select exists(select 1 from person where ident =:ident)"
+    private val existsQuery = "select exists(select 1 from person where ident=:ident)"
 
     private fun insert(ident: String, fornavn: String, etternavn: String) = session.run(
         queryOf(insert, mapOf("ident" to ident, "fornavn" to fornavn, "etternavn" to etternavn)).asUpdate
@@ -20,9 +23,12 @@ object PersonQueries {
     ) ?: throw InternalError("Failed to check if person exists")
 
     fun insertIfNotExists(ident: String, fornavn: String?, etternavn: String?) {
-        if (exists(ident))
+        if (exists(ident)) {
+            LOG.info { "User $etternavn already exists" }
             return
-        else
+        } else {
+            LOG.info { "Insert user $etternavn" }
             insert(ident, fornavn ?: "", etternavn ?: "")
+        }
     }
 }
