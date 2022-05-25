8cdc780192f3aa325cb7bdddb06b000a47e1ea48
Per Jørgen Walstrøm <per.jorgen.walstrom@nav.no>
2022-05-25 16:31:09 +0200

    Revert "Bump postgresql from 42.3.5 to 42.3.6"
    This reverts commit 0e9d19a92a752d958dbd14c344e8f6e6143684c3.

--- build.gradle.kts
+++ build.gradle.kts
@@ -75,7 +75,7 @@ dependencies {
     // DB
     implementation("org.flywaydb:flyway-core:8.5.11")
     implementation("com.zaxxer:HikariCP:5.0.1")
-    implementation("org.postgresql:postgresql:42.3.6")
+    implementation("org.postgresql:postgresql:42.3.5")
     implementation("com.github.seratch:kotliquery:1.7.0")
     // Kafka
     implementation("org.apache.kafka:kafka-clients:3.1.0")
