cd61f814f4897db48b568ee3d9a739a22589b982
Hågen Hasle <haagenhasle@gmail.com>
2022-08-25 17:42:18 +0200

    Lagt til et beskrivelse-felt og en TODO, det må leses fra Properties

--- src/main/kotlin/no/nav/tiltakspenger/mottak/søknad/BrukerregistrertTiltak.kt
+++ src/main/kotlin/no/nav/tiltakspenger/mottak/søknad/BrukerregistrertTiltak.kt
@@ -8,6 +8,7 @@ import java.time.LocalDate
 @Serializable
 data class BrukerregistrertTiltak(
     val tiltakstype: String,
+    val beskrivelse: String?,
     @Serializable(with = LocalDateSerializer::class) val tom: LocalDate?,
     val postnummer: String? = null,
     @Serializable(with = LocalDateSerializer::class) val fom: LocalDate?,
@@ -21,6 +22,10 @@ data class BrukerregistrertTiltak(
                 joarkSoknad.fakta.firstOrNull { it.key == "tiltaksliste.annetTiltak" } ?: return null
             return BrukerregistrertTiltak(
                 tiltakstype = brukerregistrertTiltakJson.value!!,
+                // TODO: I soknad_deltar_intro.json er beskrivelse feltet brukt!
+                // Ref også https://nav-it.slack.com/archives/C02BBKL9SGM/p1661434113014189?thread_ts=1661327854.713069&cid=C02BBKL9SGM
+                // beskrivelse = brukerregistrertTiltakJson.properties?.beskrivelse,
+                beskrivelse = null,
                 tom = brukerregistrertTiltakJson.properties?.tom,
                 postnummer = brukerregistrertTiltakJson.properties?.postnummer,
                 fom = brukerregistrertTiltakJson.properties?.fom,
