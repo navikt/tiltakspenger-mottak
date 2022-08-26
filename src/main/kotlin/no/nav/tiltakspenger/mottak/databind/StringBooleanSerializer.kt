8150cbbda3b34f29ed14678db69b3ee018e468bf
Richard Martinsen <github@martinsen.org>
2022-08-26 10:59:18 +0200

    StringBooleanSerializer kaster exception ved ugyldige verdier

--- src/main/kotlin/no/nav/tiltakspenger/mottak/databind/StringBooleanSerializer.kt
+++ src/main/kotlin/no/nav/tiltakspenger/mottak/databind/StringBooleanSerializer.kt
@@ -1,6 +1,5 @@
 package no.nav.tiltakspenger.mottak.databind
 
-import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.descriptors.PrimitiveKind
 import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
@@ -10,10 +9,17 @@ import kotlinx.serialization.encoding.Encoder
 
 object StringBooleanSerializer : KSerializer<Boolean> {
     private val sant = Regex("true|ja", RegexOption.IGNORE_CASE)
+    private val usant = Regex("false|nei", RegexOption.IGNORE_CASE)
     override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("StringBoolean", PrimitiveKind.STRING)
 
-    @OptIn(ExperimentalSerializationApi::class)
-    override fun deserialize(decoder: Decoder) = decoder.decodeNotNullMark() && decoder.decodeString().matches(sant)
+    override fun deserialize(decoder: Decoder): Boolean {
+        val verdi = decoder.decodeString()
+        return when {
+            verdi.matches(sant) -> true
+            verdi.matches(usant) -> false
+            else -> throw IllegalArgumentException("kun lov med true/ja/false/nei")
+        }
+    }
 
     override fun serialize(encoder: Encoder, value: Boolean) {
         TODO("Not yet implemented")
--- src/test/kotlin/no/nav/tiltakspenger/mottak/databind/StringBooleanSerializerTest.kt
+++ src/test/kotlin/no/nav/tiltakspenger/mottak/databind/StringBooleanSerializerTest.kt
@@ -6,6 +6,7 @@ import kotlinx.serialization.json.Json
 import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 internal class StringBooleanSerializerTest {
 
@@ -20,7 +21,10 @@ internal class StringBooleanSerializerTest {
         assertTrue(Json.decodeFromString<ClassForTest>("""{"bar":"Ja"}""").bar)
         assertFalse(Json.decodeFromString<ClassForTest>("""{"bar":"false"}""").bar)
         assertFalse(Json.decodeFromString<ClassForTest>("""{"bar":"nei"}""").bar)
-        assertFalse(Json.decodeFromString<ClassForTest>("""{"bar":"hei"}""").bar)
-        assertFalse(Json.decodeFromString<ClassForTest>("""{"bar":null}""").bar)
+        assertThrows<IllegalArgumentException> { Json.decodeFromString<ClassForTest>("""{"bar":"hei"}""").bar }
+        assertThrows<IllegalArgumentException> { Json.decodeFromString<ClassForTest>("""{"bar":null}""").bar }
+        assertThrows<IllegalArgumentException> { Json.decodeFromString<ClassForTest>("""{"bar":42}""").bar }
+        assertThrows<IllegalArgumentException> { Json.decodeFromString<ClassForTest>("""{"bar":true}""").bar }
+        assertThrows<IllegalArgumentException> { Json.decodeFromString<ClassForTest>("""{"bar":false}""").bar }
     }
 }
