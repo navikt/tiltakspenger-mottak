41da5f416c614bbab9f1e799fe5c37558c641dec
Per Jørgen Walstrøm <per.jorgen.walstrom@nav.no>
2022-08-26 09:54:34 +0200

    Read json from file instead of creating an object in a test

--- src/test/kotlin/no/nav/tiltakspenger/mottak/søknad/BrukerregistrertTiltakTest.kt
+++ src/test/kotlin/no/nav/tiltakspenger/mottak/søknad/BrukerregistrertTiltakTest.kt
@@ -1,47 +1,41 @@
 package no.nav.tiltakspenger.mottak.søknad
 
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
 import no.nav.tiltakspenger.mottak.joark.models.Faktum
 import no.nav.tiltakspenger.mottak.joark.models.JoarkSoknad
-import no.nav.tiltakspenger.mottak.joark.models.Properties
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.Test
+import java.io.File
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.Month
 
 internal class BrukerregistrertTiltakTest {
+    private val json = Json { ignoreUnknownKeys = true }
+
     @Test
-    fun `happy day`() {
-        val fraOgMed = LocalDate.of(2022, Month.JANUARY, 22)
-        val tilOgMed = LocalDate.of(2022, Month.FEBRUARY, 11)
-        val properties = Properties(
-            arrangoernavn = "arrangørnavn",
-            fom = fraOgMed,
-            tom = tilOgMed,
-            adresse = "en gate",
-            postnummer = "et postnummer",
-            antallDager = "12 dager",
-        )
-        val faktum = Faktum(key = "tiltaksliste.annetTiltak", value = "AMO", properties = properties)
-        val joarkSoknad = JoarkSoknad(fakta = listOf(faktum), opprettetDato = LocalDateTime.MIN)
+    fun `søknad med brukerregistrert tiltak`() {
+        val jsonFromFile = File("src/test/resources/soknad_uten_tiltak_fra_arena.json").readText()
+        val joarkSoknad = json.decodeFromString<JoarkSoknad>(jsonFromFile)
 
         val tiltak = BrukerregistrertTiltak.fromJoarkSoknad(joarkSoknad)
 
         assertNotNull(tiltak)
         assertEquals("AMO", tiltak?.tiltakstype)
 //        assertEquals("beskrivelse", tiltak?.beskrivelse)
-        assertEquals(fraOgMed, tiltak?.fom)
-        assertEquals(tilOgMed, tiltak?.tom)
-        assertEquals("en gate", tiltak?.adresse)
-        assertEquals("et postnummer", tiltak?.postnummer)
-        assertEquals("arrangørnavn", tiltak?.arrangoernavn)
-        assertEquals(12, tiltak?.antallDager)
+        assertEquals(LocalDate.of(2022, Month.MARCH, 1), tiltak?.fom)
+        assertEquals(LocalDate.of(2022, Month.MARCH, 31), tiltak?.tom)
+        assertEquals("Storgata 1", tiltak?.adresse)
+        assertEquals("0318", tiltak?.postnummer)
+        assertEquals("Tull og tøys AS", tiltak?.arrangoernavn)
+        assertEquals(5, tiltak?.antallDager)
     }
 
     @Test
-    fun `tiltak is null when key is not found`() {
+    fun `søknad uten brukerregistrert tiltak`() {
         val faktum = Faktum(key = "en faktumnøkkel vi ikke har noe forhold til")
         val joarkSoknad = JoarkSoknad(fakta = listOf(faktum), opprettetDato = LocalDateTime.MIN)
 
