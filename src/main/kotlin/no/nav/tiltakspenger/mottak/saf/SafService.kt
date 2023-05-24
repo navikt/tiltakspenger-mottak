44bed6e0c16c9391be9184e59777e8325bfaac52
Hågen Hasle <haagenhasle@gmail.com>
2023-05-24 21:39:52 +0200

    Ignorerer nye søknader

--- src/main/kotlin/no/nav/tiltakspenger/mottak/saf/SafService.kt
+++ src/main/kotlin/no/nav/tiltakspenger/mottak/saf/SafService.kt
@@ -7,6 +7,11 @@ private val LOG = KotlinLogging.logger {}
 private val SECURELOG = KotlinLogging.logger("tjenestekall")
 
 class SafService(private val safClient: SafClient) {
+
+    companion object {
+        private const val FILNAVN_NY_SØKNAD = "tiltakspengersoknad.json"
+    }
+
     suspend fun hentSøknad(journalpostId: String): Søknad? {
         LOG.info { "Henter metadata for journalpost med journalpostId $journalpostId" }
         val metadata = safClient.hentMetadataForJournalpost(journalpostId)
@@ -15,6 +20,10 @@ class SafService(private val safClient: SafClient) {
             return null
         }
         LOG.info { "Henter søknad med dokumentInfoId ${metadata.dokumentInfoId}" }
+        if (metadata.filnavn == FILNAVN_NY_SØKNAD) {
+            LOG.info { "Journalpost med id $journalpostId ignoreres, er ny søknad" }
+            return null
+        }
         val json = safClient.hentSoknad(metadata)
         LOG.info { "Hentet søknad med dokumentInfoId ${metadata.dokumentInfoId}, se secure-log for detaljer" }
         SECURELOG.info { "Hentet søknad $json" }
