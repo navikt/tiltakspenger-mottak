2131b7fe0ccc71b32c8333399de5c5fcc50c5c44
Hågen Hasle <haagenhasle@gmail.com>
2023-11-29 21:12:16 +0100

    R Oppgradert til Gradle 8.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
     // id("com.github.ben-manes.versions") version "0.47.0"
-    id("com.diffplug.spotless") version "6.22.0"
+    id("com.diffplug.spotless") version "6.23.1"
     // id("ca.cutterslade.analyze") version "1.9.1"
 }
 
